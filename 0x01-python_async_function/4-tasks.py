799b9b8a01361e00d87d256036a8e95aa592904e
mee-rich <Jamessuyi@gmail.com>
2024-10-18 10:45:24 +0100

    done

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-asyn def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Spawm wait_random n times"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
